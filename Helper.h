374d1768bb17d76b756e8281ac3e4a8a348fe0f8
Dillon Welch <daw0328@gmail.com>
2012-05-05 22:02:01 -0500

    Misc comments.

--- Helper.h
+++ Helper.h
@@ -8,24 +8,33 @@
 
 #include "Global.h"
 
+// Functions that return an int.
 int boolNtoInt(int size, bool word[]);
 int boolQuartetToInt(int size, int lengthOfBinaryNumber, int quartetNum, bool word[]);
+
 int overflowTest(int size, bool word[]);
 
+// Functions that return nothing.
 void bitwiseAdd(int sizeA, bool wordA[], int sizeB, bool wordB[], bool result[]);
 void bitwiseAddImm(int sizeA, bool wordA[], int addImm, bool result[]);
 void bitwiseNot(int size, bool word[], bool result[]);
+
 void calculateInstructionStrings(bool memory[TOTAL_MEM_SIZE][WORD_SIZE]);
 void copyBooleanArray(bool src[], bool dest[]);
+
 void intToBoolN(int number, int size, bool word[]);
 void intToBoolQuartet(int number, int size, int quartetNum, bool word[]);
+
 void printBoolArray(int size, bool word[]);
 void printMemoryLocation(int index, bool memory[TOTAL_MEM_SIZE][WORD_SIZE]);
 void printProgramCounterMemory(bool memory[TOTAL_MEM_SIZE][WORD_SIZE]);
 void printRegisters(bool memory[TOTAL_MEM_SIZE][WORD_SIZE]);
 void printUserDisplay(bool memory[TOTAL_MEM_SIZE][WORD_SIZE]);
+
 void setMemoryBoolArray(int location, bool number[WORD_SIZE], bool memory[TOTAL_MEM_SIZE][WORD_SIZE]);
 void setMemoryInt(int location, int number, bool memory[TOTAL_MEM_SIZE][WORD_SIZE]);
 void switchSign(int size, bool number[]);
+
 void waitForEnter();
+
 void zeroBoolArray(int size, bool word[]);
